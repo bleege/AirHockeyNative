3fa4cc06f02b055c6ff2a50e3ffef32494b38d8e
Brad Leege <bleege@gmail.com>
2015-11-28 23:06:13 -0600

    Basic OpenGL and Renderer infrastructure setup

---
+++ android/app/src/main/java/com/bradleege/airhockeynative/RendererWrapper.java
@@ -0,0 +1,25 @@
+package com.bradleege.airhockeynative;
+
+import android.opengl.GLSurfaceView;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+import static android.opengl.GLES20.*;
+
+public class RendererWrapper implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // Clear Color set to Blue
+        glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // No-op
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
