9ecf60a0bf8e3be59b95a4563272645d397f2418
Brad Leege <bleege@gmail.com>
2015-12-06 10:12:58 -0600

    Initial creation of common c++ code and javah generated JNI wrapper

---
+++ android/app/src/main/java/com/bradleege/airhockeynative/GameLibJNIWrapper.java
@@ -0,0 +1,15 @@
+package com.bradleege.airhockeynative;
+
+public class GameLibJNIWrapper {
+
+    static {
+        System.loadLibrary("game");
+    }
+
+    public static native void on_surface_created();
+
+    public static native void on_surface_changed(int width, int height);
+
+    public static native void on_draw_frame();
+
+}
